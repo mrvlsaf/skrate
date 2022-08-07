3c2fa7c899bf93de3d9d8c707cc0193f1484207a
Devraj Singh <dsdevrajsingh55555@gmail.com>
2022-08-08 01:16:39 +0530

    handled all the api responses

--- src/components/NewJobs.js
+++ src/components/NewJobs.js
@@ -2,52 +2,31 @@ import './Home.css';
 import upcoming_img from "../data/upcoming-image.png";
 import upcoming_next from "../data/upcoming-arrow.png";
 
-const NewJobs = () => {
+const NewJobs = (props) => {
+
+    const data = props.jobs;
+
     return (
         <>
             <div className='third-container'>
                 <div className='home-heading'>New Jobs</div>
                 <div className='job-item-cont'>
-                    <div className='job-card'>
-                        <img src={upcoming_img} alt="image" />
-                        <div>
-                            <span className='f500'>Flutter Developer</span>
-                            <span>Google</span>
-                            <span>Remote</span>
-                        </div>
-                        <span>2 Hrs Ago</span>
-                        <img src={upcoming_next} alt="image" />
-                    </div>
-                    <div className='job-card'>
-                        <img src={upcoming_img} alt="image" />
-                        <div>
-                            <span className='f500'>Flutter Developer</span>
-                            <span>Google</span>
-                            <span>Remote</span>
-                        </div>
-                        <span>2 Hrs Ago</span>
-                        <img src={upcoming_next} alt="image" />
-                    </div>
-                    <div className='job-card'>
-                        <img src={upcoming_img} alt="image" />
-                        <div>
-                            <span className='f500'>Flutter Developer</span>
-                            <span>Google</span>
-                            <span>Remote</span>
-                        </div>
-                        <span>2 Hrs Ago</span>
-                        <img src={upcoming_next} alt="image" />
-                    </div>
-                    <div className='job-card'>
-                        <img src={upcoming_img} alt="image" />
-                        <div>
-                            <span className='f500'>Flutter Developer</span>
-                            <span>Google</span>
-                            <span>Remote</span>
-                        </div>
-                        <span>2 Hrs Ago</span>
-                        <img src={upcoming_next} alt="image" />
-                    </div>
+                    {
+                        data.map((item, index) => {
+                            return (
+                                <div className='job-card'>
+                                    <img src={upcoming_img} alt="image" />
+                                    <div>
+                                        <span className='f500'>{item.role}</span>
+                                        <span>{item.organization_name}</span>
+                                        <span>{item.location}</span>
+                                    </div>
+                                    <span>{item.date_posted}</span>
+                                    <img src={upcoming_next} alt="image" />
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </>
