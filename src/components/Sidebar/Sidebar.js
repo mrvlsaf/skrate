f4a69c7a53b982f835fb4f75418725f36135a0a9
Devraj Singh <dsdevrajsingh55555@gmail.com>
2022-08-08 15:19:30 +0530

    sidebar tab functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,24 @@
 import './Sidebar.css';
 import home from "../../data/home-sidebar.png";
 import shuffle from "../../data/shuffle-sidebar.png";
+import { useState } from 'react';
 
 const Sidebar = () => {
+
+    const [activeClass, setActiveClass] = useState("");
+
+    const setActive = (event) => {
+        setActiveClass(event.target.id)
+    }
+
     return (
         <>
             <div className='sidebar'>
-                <div className='active'>
+                <div className={activeClass==="1" ? "active" : undefined} id={"1"} onClick={setActive}>
                     <img src={home} alt="home-image" />
                     <span>Home</span>
                 </div>
-                <div>
+                <div className={activeClass==="2" ? "active" : undefined} id={"2"} onClick={setActive}>
                     <img src={shuffle} alt="home-image" />
                     <span>Shuffle</span>
                 </div>
